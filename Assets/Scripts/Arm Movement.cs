581ff6cdcfcdfae4e1b470f162ff27f9055a22d0
Ben Herman <bherman24@dtechhs.org>
2024-01-11 12:17:28 -0800

    you can move hands

--- Assets/Scripts/Arm Movement.cs
+++ Assets/Scripts/Arm Movement.cs
@@ -7,9 +7,26 @@ public class ArmMovement : NetworkBehaviour
 {
     [SerializeField] private GameObject leftHand;
     [SerializeField] private GameObject rightHand;
+    private GameObject movingHand;
+
+    private void Awake() {
+        movingHand = leftHand;
+    }
 
     void Update()
     {
-        leftHand.GetComponent<ConfigurableJoint>().targetPosition += new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0) / 10;
-    }
+        if (Input.GetKey(KeyCode.A))
+            movingHand = leftHand;
+        if (Input.GetKey(KeyCode.D))
+            movingHand = rightHand;
+    
+        var newHandPos = movingHand.GetComponent<ConfigurableJoint>().targetPosition + new Vector3(
+            Input.GetAxis("Mouse X"),
+            0,
+            Input.GetAxis("Mouse Y")
+            ) / 5;
+        if (newHandPos.magnitude > 1)
+            newHandPos = newHandPos.normalized * 1f;
+        movingHand.GetComponent<ConfigurableJoint>().targetPosition = newHandPos;
+    }   
 }
