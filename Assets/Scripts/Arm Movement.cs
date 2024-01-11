27537477aacb2feb99abaff1b4e4fe8d08cb9bdd
ItchyTrack <116318357+BenHerman24@users.noreply.github.com>
2024-01-11 01:16:56 -0800

    fix

---
+++ Assets/Scripts/Arm Movement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class ArmMovement : NetworkBehaviour
+{
+    [SerializeField] private GameObject leftArm;
+    [SerializeField] private GameObject rightArm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
