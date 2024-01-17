df3cdf74b4d2f64a9f68c64f1c7debcae8b3e6aa
Ben Herman <bherman24@dtechhs.org>
2024-01-17 10:03:38 -0800

    psuh

--- Assets/Scripts/Tools/Tool.cs
+++ Assets/Scripts/Tools/Tool.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public abstract class Tool : MonoBehaviour
+public abstract class Tool : NetworkBehaviour
 {
     [SerializeField] private bool isToggle;
     private GameObject grabberGameObject;
-    private bool isOn;
+    private readonly NetworkVariable<bool> isOn = new(false);
     public void Grabbed(GameObject grabber)
     {
         if (grabberGameObject == null)
@@ -21,27 +22,35 @@ public abstract class Tool : MonoBehaviour
 
     public void Update()
     {
-        if (grabberGameObject != null && grabberGameObject.TryGetComponent(out Player player) && player.IsOwner)
-        {
-            if (isToggle)
+        if (IsOwner) {
+            if (grabberGameObject != null && grabberGameObject.TryGetComponent(out Player player) && player.IsOwner)
             {
-                if (Input.GetKeyDown(KeyCode.F))
-                    isOn = !isOn;
-            }
-            else
-            {
-                if (Input.GetKey(KeyCode.F))
-                    isOn = true;
-            }
-        }
-        if (isOn)
-        {
-            Use();
-            if (!isToggle)
-            {
-                isOn = false;
+                if (isToggle)
+                {
+                    if (Input.GetKeyDown(KeyCode.F))
+                        isOn.Value = !isOn.Value;
+                }
+                else
+                {
+                    if (Input.GetKey(KeyCode.F))
+                        isOn.Value = true;
+                }
             }
         }
+        if (IsServer){
+            if (isOn.Value)
+            {
+                Use();
+                if (!isToggle)
+                {
+                    isOn.Value = false;
+                }
+            }
+        }
+    }
+
+    private void Destroy() {
+        
     }
 
     protected abstract void Use();
