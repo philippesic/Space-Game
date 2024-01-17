bd172849c13f731c99369ee86a5950d8294fbb15
ItchyTrack <116318357+BenHerman24@users.noreply.github.com>
2024-01-17 00:21:23 -0800

    push

--- Assets/Scripts/Network UI.cs
+++ Assets/Scripts/Network UI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using UnityEngine;
@@ -9,14 +10,18 @@ public class NetworkUI : MonoBehaviour
 {
     [SerializeField] private Button host;
     [SerializeField] private Button client;
+    [SerializeField] private TextMeshProUGUI ipText;
+    [SerializeField] private TMP_InputField ipInput;
 
     private void Awake() {
+        ipText.text = IPManager.GetLocalIPAddress();
         host.onClick.AddListener(() => {
-            //NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = IPManager.GetLocalIPAddress();
+            NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = IPManager.GetLocalIPAddress();
             NetworkManager.Singleton.StartHost();
             Destroy(gameObject);
         });
         client.onClick.AddListener(() => {
+            NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = ipInput.text;
             NetworkManager.Singleton.StartClient();
             Destroy(gameObject);
         });
