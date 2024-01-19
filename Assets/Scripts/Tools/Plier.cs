bd7cf820f1eced23c6f2c4aafe903573c16e6aff
Philip Pesic <116986036+philippesic@users.noreply.github.com>
2024-01-19 10:20:06 -0800

    wow

--- Assets/Scripts/Tools/Plier.cs
+++ Assets/Scripts/Tools/Plier.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 public class Plier : Tool
 {
 
-
+    [SerializeField] private Transform pos;
     protected override void Use()
     {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, 0.1f);
+        Collider[] colliders = Physics.OverlapSphere(pos.position, 0.05f);
         foreach (Collider collider in colliders)
         {
-            if (collider.TryGetComponent(out Wire wire))
+            if (collider.TryGetComponent(out Wire wire) || collider.transform.parent.TryGetComponent(out wire))
             {
                 wire.GetPliered();
             }
