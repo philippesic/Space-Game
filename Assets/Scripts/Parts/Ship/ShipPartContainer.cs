fbf222b314d4ae252a9a7267667faaa1ca0568b9
Philip Pesic <116986036+philippesic@users.noreply.github.com>
2024-01-22 13:00:16 -0800

    buh

---
+++ Assets/Scripts/Parts/Ship/ShipPartContainer.cs
@@ -0,0 +1,21 @@
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEditor;
+// using UnityEngine;
+
+// public class ShipPartContainer : MonoBehaviour
+// {
+//     private List<ShipPart> parts = new();
+
+//     public void AddPart(GameObject prefab, Vector3 position, Quaternion rotation)
+//     {
+//         gameObject
+//         part.transform.parent = transform;
+//         part.transform.position = position;
+//         part.transform.rotation = rotation;
+//         parts.Add(part);
+//     }
+// =
+
+// }
