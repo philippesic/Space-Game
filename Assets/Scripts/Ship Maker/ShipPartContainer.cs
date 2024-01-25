0bdde386a8565c05d0d9e9ae948fd3c020fa37f7
Ben Herman <bherman24@dtechhs.org>
2024-01-25 09:34:45 -0800

    fix spawn

--- Assets/Scripts/Ship Maker/ShipPartContainer.cs
+++ Assets/Scripts/Ship Maker/ShipPartContainer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class ShipPartContainer : MonoBehaviour
@@ -14,6 +15,7 @@ public class ShipPartContainer : MonoBehaviour
     public void AddPart(GameObject prefab, Vector3 position, Quaternion rotation)
     {
         GameObject partGameObject = Instantiate(prefab, position, rotation, transform);
+        if (partGameObject.TryGetComponent(out NetworkObject networkObject)) networkObject.Spawn();
         if (partGameObject.TryGetComponent(out ShipPart shipPart))
         {
             partGameObject.transform.position = position * shipPartScale;
