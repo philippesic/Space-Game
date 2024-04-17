d6a0781a318d57484ad70fd256462108c33e3ab1
Philip Pesic <philippesic06@gmail.com>
2024-04-17 09:08:48 -0700

    Update Laser.cs

--- Assets/Scripts/Parts/Tools/Laser.cs
+++ Assets/Scripts/Parts/Tools/Laser.cs
@@ -6,8 +6,9 @@ public class Laser : Tool
 {
     protected override void Use()
     {
-        Ray ray = new(transform.position, transform.forward);
 
+        Ray ray = new(transform.position, transform.forward);
+        Debug.DrawRay(transform.position, transform.forward, Color.red);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             if (hit.collider.gameObject.TryGetComponent(out Interactable interactable) || hit.collider.gameObject.transform.parent.TryGetComponent(out interactable))
